f273e757ad5ab6360919d283715543b9ab9e7540
josue-cifuentes <josue8cc@hotmail.com>
2026-04-16 13:32:06 -0600

    Fix PrismaNeon adapter using correct neon Pool

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,16 @@
 import path from "node:path";
 import { defineConfig } from "prisma/config";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
 
 export default defineConfig({
   earlyAccess: true,
   schema: path.join("prisma", "schema.prisma"),
   migrate: {
     async adapter() {
-      const { PrismaNeon } = await import("@prisma/adapter-neon");
-      const { neonConfig, Pool } = await import("@neondatabase/serverless");
-      
-      // Optional: if using a connection string with pooling
       const pool = new Pool({ connectionString: process.env.DATABASE_URL });
       return new PrismaNeon(pool);
     },
